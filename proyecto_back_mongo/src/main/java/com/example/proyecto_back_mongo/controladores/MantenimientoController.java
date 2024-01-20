21aad49bafb5434e3534f9aeabb59532e6757851
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-20 16:50:13 -0500

    Update MantenimientoController.java

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/controladores/MantenimientoController.java
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/controladores/MantenimientoController.java
@@ -3,10 +3,9 @@ package com.example.proyecto_back_mongo.controladores;
 import com.example.proyecto_back_mongo.documentos.MantenimientoCollection;
 import com.example.proyecto_back_mongo.servicios.MantenimientoServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/proyecto/mongo")
@@ -17,5 +16,10 @@ public class MantenimientoController {
     @PostMapping("/guardar")
     public MantenimientoCollection guardar(@RequestBody MantenimientoCollection collection){
         return services.guardar(collection);
+    }
+
+    @GetMapping("/listar")
+    public List<MantenimientoCollection> listar(){
+        return services.listar();
     }
 }
