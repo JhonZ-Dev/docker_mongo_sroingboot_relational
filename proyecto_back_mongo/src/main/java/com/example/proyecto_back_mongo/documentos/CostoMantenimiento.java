a8f2d0b9ed310a9bbc7be56b13a4723a020bf6bb
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-20 17:14:44 -0500

    Update CostoMantenimiento.java

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/documentos/CostoMantenimiento.java
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/documentos/CostoMantenimiento.java
@@ -1,4 +1,25 @@
 package com.example.proyecto_back_mongo.documentos;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document(collection = "Costos")
+@Data
 public class CostoMantenimiento {
+
+    @Id
+    @Indexed(unique = true)
+    private int id_costos;
+    private String descripcion;
+    private String materiales_implementados;
+    private Double costo_inicial = 0.0;
+    private LocalDate fecha_llegada_problema;
+    private LocalDate fecha_posiblesolucion_problema;
+    private String precio_final_por_dias;
+
+
 }
