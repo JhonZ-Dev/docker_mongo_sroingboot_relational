508558b3d7ad9f326002956f4e3edc087c77f51d
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-20 14:09:20 -0500

    Create Proyect

---
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/ProyectoBackMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_back_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBackMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBackMongoApplication.class, args);
+	}
+
+}
---
+++ proyecto_back_mongo/src/test/java/com/example/proyecto_back_mongo/ProyectoBackMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_back_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBackMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
