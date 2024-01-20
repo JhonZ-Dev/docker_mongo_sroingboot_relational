07a7d571637dac50dcb114a88c911cfa2828a39e
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-20 17:16:48 -0500

    AccionesNecesarias

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/repositorios/CostoRepositorio.java
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/repositorios/CostoRepositorio.java
@@ -1,4 +1,13 @@
 package com.example.proyecto_back_mongo.repositorios;
 
-public interface CostoRepositorio {
+import com.example.proyecto_back_mongo.documentos.CostoMantenimiento;
+import com.example.proyecto_back_mongo.documentos.MantenimientoCollection;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+
+public interface CostoRepositorio extends MongoRepository<CostoMantenimiento, Integer> {
+    @Query(value = "{}", sort = "{id_costos: -1}")
+    Optional<CostoMantenimiento> findTopByOrderByIdCostosDesc();
 }
