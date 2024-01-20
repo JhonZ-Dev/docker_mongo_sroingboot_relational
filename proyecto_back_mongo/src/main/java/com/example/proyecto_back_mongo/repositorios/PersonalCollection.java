c81d0d45288575445ca3765b091ea041b585362f
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-20 16:46:48 -0500

    Update PersonalCollectionRepositorio.java

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/repositorios/PersonalCollection.java
+++
@@ -1,4 +0,0 @@
-package com.example.proyecto_back_mongo.repositorios;
-
-public interface PersonalCollection {
-}
---
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/repositorios/PersonalCollectionRepositorio.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_back_mongo.repositorios;
+
+import com.example.proyecto_back_mongo.documentos.MantenimientoCollection;
+import com.example.proyecto_back_mongo.documentos.PersonalCollection;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+
+public interface PersonalCollectionRepositorio extends MongoRepository<PersonalCollection, Integer>
+{
+    @Query(value = "{}", sort = "{id_personal: -1}")
+    Optional<MantenimientoCollection> findTopByOrderByIdPersonalDesc();
+}
