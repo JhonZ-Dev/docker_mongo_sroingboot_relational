17b3296521a13a6771d10c29e6cee222edc9f317
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-20 14:27:41 -0500

    Update MantenimientoRepositorio.java

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/repositorios/MantenimientoRepositorio.java
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/repositorios/MantenimientoRepositorio.java
@@ -3,5 +3,6 @@ package com.example.proyecto_back_mongo.repositorios;
 import com.example.proyecto_back_mongo.documentos.MantenimientoCollection;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+
 public interface MantenimientoRepositorio extends MongoRepository<MantenimientoCollection, Integer> {
 }
