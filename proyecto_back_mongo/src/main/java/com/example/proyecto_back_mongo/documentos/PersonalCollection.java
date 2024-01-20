f887fe20f0cd54e17f828a050640232d0d722bc9
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-20 16:45:00 -0500

    Update PersonalCollection.java

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/documentos/PersonalCollection.java
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/documentos/PersonalCollection.java
@@ -1,4 +1,20 @@
 package com.example.proyecto_back_mongo.documentos;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document(collection = "Mantenimientos")
+@Data
 public class PersonalCollection {
+    @Id
+    @Indexed(unique = true)
+    private int id_personal;
+    private String edad;
+    private LocalDate fecha_nacimiento;
+    private String nombre, apellido, telefono, email;
+
 }
