12d02364ccf4c72a999e57758b9cef52a4368d16
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-20 14:21:23 -0500

    Update MantenimientoCollection.java

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/documentos/MantenimientoCollection.java
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/documentos/MantenimientoCollection.java
@@ -1,4 +1,16 @@
 package com.example.proyecto_back_mongo.documentos;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "coleccion_mantenimiento")
 public class MantenimientoCollection {
+
+    @Id
+    private int id_mantenimineto;
+    private String problema;
+    private String solucion;
+    private String fecha_llegada_problema;
+    private String fecha_solucion_problema;
+    private String dias_espera_solucion;
 }
