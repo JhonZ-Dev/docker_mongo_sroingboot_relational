19d5fbe258b1c1b29d22b1025ec780c3074f9e15
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-21 16:01:38 -0500

    Create UsuariosMantenimientoColletion.java

---
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/documentos/UsuariosMantenimientoColletion.java
@@ -0,0 +1,19 @@
+package com.example.proyecto_back_mongo.documentos;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.annotation.Collation;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Usuarios")
+@Data
+public class UsuariosMantenimientoColletion {
+    @Id
+    @Indexed(unique = true)
+    private int id_usuario;
+    private String nombre_usuario;
+    private String contrasenia_usuario;
+    private String rol_usuario;
+
+}
