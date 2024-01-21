451993610dd4579e74aa229d30292b45fabdcafa
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-21 17:46:56 -0500

    Update UsuarioControlador.java

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/controladores/UsuarioControlador.java
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/controladores/UsuarioControlador.java
@@ -3,10 +3,10 @@ package com.example.proyecto_back_mongo.controladores;
 import com.example.proyecto_back_mongo.documentos.UsuariosMantenimientoColletion;
 import com.example.proyecto_back_mongo.servicios.UsuarioServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/datos/restTemplate")
@@ -17,6 +17,28 @@ public class UsuarioControlador {
     @PostMapping("/insertar")
     public UsuariosMantenimientoColletion guardar(@RequestBody UsuariosMantenimientoColletion usuariosMantenimientoColletion){
         return services.guardarResTemplate(usuariosMantenimientoColletion);
+    }
+
+    @GetMapping("/listar")
+    public List<UsuariosMantenimientoColletion> listarUsuarios(){
+        return services.listar();
+    }
+
+    @PutMapping("/editar/{id_usuario}")
+    public UsuariosMantenimientoColletion editarUsuarios(@RequestBody UsuariosMantenimientoColletion usuariosMantenimientoColletion,
+                                                         @PathVariable Integer id_usuario){
+        return services.editar(usuariosMantenimientoColletion,id_usuario);
+    }
+
+    @DeleteMapping("/eliminar/{id_usuarios}")
+    public void eliminarUsuarios(@PathVariable Integer id_usuarios){
+        services.eliminar(id_usuarios);
+    }
+
+    //trearPorid
+    @GetMapping("/traer-por-id/{id_usuario}")
+    public Optional<UsuariosMantenimientoColletion> traerPorSuId(@PathVariable Integer id_usuario){
+        return services.traerPorId(id_usuario);
     }
 
 }
