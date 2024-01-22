72a429e152242c523b965f257afa617edf48f4e4
JhonZ-Dev <jhonmacias1999@gmail.com>
2024-01-21 20:49:28 -0500

    Update UsuarioControlador.java

--- proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/controladores/UsuarioControlador.java
+++ proyecto_back_mongo/src/main/java/com/example/proyecto_back_mongo/controladores/UsuarioControlador.java
@@ -41,4 +41,9 @@ public class UsuarioControlador {
         return services.traerPorId(id_usuario);
     }
 
+    @GetMapping("/traer-datos-desde-postgres")
+    public String traerListaUsuarios(){
+        return services.traerListaUsuarios();
+    }
+
 }
